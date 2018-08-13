514d26cd6a0c3533f53d33270a9ac037d02f8664
laurenshareshian <laurenshareshian@gmail.com>
2018-08-13 16:25:31 -0700

    getting user data in successfully

--- js/components/input-form-app.js
+++ js/components/input-form-app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import peopleApi from '../services/people-api.js';
+import ProfileInputForm from './profile-input-form.js';
 import html from '../html.js';
 // import Header from './header.js';
 // import Footer from './footer.js';
@@ -8,35 +9,6 @@ import html from '../html.js';
 let template = function() {
     return html`    
     <main>
-        <form>
-            <label>Name:</label>
-                <input name="name" type="text"><br>
-            <label>Income</label>
-                <select id="income">
-                    <option value="0-50K">0 - 50K</option>
-                    <option value="50-100K">50 - 100K</option>
-                    <option value="100-150K">100 - 150K</option>
-                    <option value="150-200K">150 - 200K</option>
-                    <option value="200K+">200K+</option>
-                </select><br>
-            <label>Fertility</label>
-                <input type="range" min="1" max="5"><br>
-            <label>Favorite Book</label>
-                <select id="book">
-                    <option value="If I Did It by OJ Simpson">If I Did It by OJ Simpson</option>
-                    <option value="Awaken the Giant Within by Tony Robbins">Awaken the Giant Within by Tony Robbins</option>
-                    <option value="Atlas Shrugged by Ayn Rand">Atlas Shrugged by Ayn Rand</option>
-                    <option value="The Bible by God">The Bible by God</option>
-                    <option value="Leaves of Grass by Walt Whitman">Leaves of Grass by Walt Whitman</option>
-                    <option value="An Inconvenient Truth by Al Gore">An Inconvenient Truth by Al Gore</option>
-                </select><br>
-            <label>How would your best friend describe you?</label>
-                <textarea name="summary" rows="3" cols="30"></textarea><br>
-            <label>Insert a URL of a recent picture of yourself.</label>
-                <input name="pic" type="text"><br>
-            <label>What ladder number do you think you are?</label>
-                <input type="range" min="1" max="5"><br>
-        </form>
     </main>`;
 
 };
@@ -45,13 +17,19 @@ export default class App {
     constructor() {
         this.people = peopleApi.get();
     }
-
     render() {
-        console.log('hello');
-        // console.log(this.people[0]);
-
         let dom = template();
-        console.log('dom', dom);
+
+        this.main = dom.querySelector('main');
+        let profileInputForm = new ProfileInputForm({
+            people: this.people,
+            onSubmit: function(people) {
+                profileInputForm.update({
+                    people: people
+                });
+            }
+        });
+        this.main.appendChild(profileInputForm.render());
         return dom;
     }
 }
---
+++ js/components/profile-input-form.js
@@ -0,0 +1,77 @@
+'use strict';
+
+import html from '../html.js';
+// import Header from './header.js';
+// import Footer from './footer.js';
+
+let template = function() {
+    return html`    
+    <form>
+        <label>Name:</label>
+            <input name="name" type="text"><br>
+        <label>Income</label>
+            <select id="income">
+                <option value="0-50K">0 - 50K</option>
+                <option value="50-100K">50 - 100K</option>
+                <option value="100-150K">100 - 150K</option>
+                <option value="150-200K">150 - 200K</option>
+                <option value="200K+">200K+</option>
+            </select><br>
+        <label>Fertility</label>
+            <input type="range" min="1" max="5" id="fertility"><br>
+        <label>Favorite Book</label>
+            <select id="book">
+                <option value="If I Did It by OJ Simpson">If I Did It by OJ Simpson</option>
+                <option value="Awaken the Giant Within by Tony Robbins">Awaken the Giant Within by Tony Robbins</option>
+                <option value="Atlas Shrugged by Ayn Rand">Atlas Shrugged by Ayn Rand</option>
+                <option value="The Bible by God">The Bible by God</option>
+                <option value="Leaves of Grass by Walt Whitman">Leaves of Grass by Walt Whitman</option>
+                <option value="An Inconvenient Truth by Al Gore">An Inconvenient Truth by Al Gore</option>
+            </select><br>
+        <label>How would your best friend describe you?</label>
+            <textarea name="summary" rows="3" cols="30"></textarea><br>
+        <label>Insert a URL of a recent picture of yourself.</label>
+            <input name="pic" type="text"><br>
+        <label>What ladder number do you think you are?</label>
+            <input type="range" min="1" max="5" id="ladder"><br>
+        <input type="submit">
+    </form>`;
+
+};
+
+export default class ProfileInputForm {
+    constructor(props) {
+        this.people = props.people;
+        this.onSubmit = props.onSubmit;
+    }
+
+    update(props) {
+        this.people = props.people;
+        console.log('inside update');
+    }
+    render() {
+        let dom = template();
+
+        this.form = dom.querySelector('form');
+
+        this.form.addEventListener('submit', (event) => {
+            // #1 Prevent default posting of the form
+            event.preventDefault();
+
+            // #2 Gather up data
+            let elements = this.form.elements;
+            let name = elements.name.value;
+            let income = elements.income.value;
+            let fertility = elements.fertility.value;
+            let book = elements.book.value;
+            let bio = elements.summary.value;
+            let pic = elements.pic.value;
+            let ladder = elements.ladder.value;
+
+            console.log(name, income, fertility, book, bio, pic, ladder);
+        });
+
+        return dom;
+
+    }
+}
