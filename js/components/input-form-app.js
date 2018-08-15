504dabb16ad9d9c4959a079a5c16bf97dace0cd0
laurenshareshian <laurenshareshian@gmail.com>
2018-08-14 17:38:02 -0700

    footer and loser components working

--- js/components/input-form-app.js
+++ js/components/input-form-app.js
@@ -4,7 +4,7 @@ import peopleApi from '../services/people-api.js';
 import ProfileInputForm from './profile-input-form.js';
 import html from '../html.js';
 import Header from './header.js';
-// import Footer from './footer.js';
+import Footer from './footer.js';
 
 let template = function() {
     return html`    
@@ -34,8 +34,8 @@ export default class App {
         this.main.appendChild(profileInputForm.render());
 
         // append footer
-        // let footer = new Footer();
-        // this.main.appendChild(footer.render());
+        let footer = new Footer();
+        this.main.appendChild(footer.render());
 
         return dom;
     }
