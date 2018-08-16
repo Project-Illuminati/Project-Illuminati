0277a05de162a38335f76e635d7338ad7d3695a4
Claire Flanagan <claireflanagan@gmail.com>
2018-08-16 14:55:34 -0700

    fixing bugs

--- js/components/choices-form.js
+++ js/components/choices-form.js
@@ -9,7 +9,8 @@ import Pic from './pic.js';
 let template = function() {
     return html`
     <div class="form-contents">    
-        <div class="choice-form">
+        <div class="outer-choice-form">
+            <div class="choice-form"></div>
             <label><button class="done">Submit</label>
         </div>
         <label><button class="redirect" id="redirect" onclick="window.location.href='../../results.html'">Show me my matches!</label>
@@ -29,6 +30,7 @@ export default class ChoicesForm {
         let dom = template();
 
         this.form = dom.querySelector('div.choice-form');
+        this.containingFormDiv = dom.querySelector('.outer-choice-form');
 
         for(let i = 0; i < this.suitors.length; i++) {
             let bio = new Bio({
@@ -60,6 +62,7 @@ export default class ChoicesForm {
 
     update(props) {
         this.people = props.people;
+        this.containingFormDiv.lastElementChild.remove();
         while(this.form.lastElementChild){
             this.form.lastElementChild.remove();
         }
