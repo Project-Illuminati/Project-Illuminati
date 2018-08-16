92457ac33fa3469c8a4b05540d720458f3df26ff
warrioryoko <miangpellegri@gmail.com>
2018-08-16 15:07:59 -0700

    Minor changes to the about us page.

--- js/components/aboutus-app.js
+++ js/components/aboutus-app.js
@@ -11,49 +11,33 @@ let template = function() {
             <div class="container">
                 <img class="bio-pic" src="../img/laurenpic.png">
 
-                <p class="bio-text">Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit.  </p>
+                <p class="bio-text">I'm an expert in celebrities.</p>
 
-                <p class="bio-text">My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+                <p class="bio-text">I spend all of my time reading Us Weekly and TMZ.</p>
 
-                <p class="bio-text">Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. </p>
-
-                <p class="bio-text">The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. </p>
-
-                <p>The game has changed. The word is out. And you... are a killer. Apparently it's all over town. Somebody crossed you, you got angry, you crushed their skull with an ATM machine. Who cares! Just as long as it's our competitors who believe it and not the police. </p>
-                <p>Don't you see how great this is? You, you are a... Jesse look at me. You... are a blowfish. A blowfish! Think about it. Small in stature, not swift, not cunning. Easy prey for predators but the blowfish has a secret weapon doesn't he. Doesn't he? What does the blowfish do, Jesse. What does the blowfish do? The blowfish puffs up, okay?</p>
-                <p>The blowfish puffs himself up four, five times larger than normal and why? Why does he do that? So that it makes him intimidating, that's why. Intimidating! So that the other, scarier fish are scared off. And that's you! You are a blowfish. You see it's just all an illusion. You see it's... it's nothing but air. Now... who messes with the blowfish, Jesse? You're damn right. You are a blowfish. Say it again. Say it like you mean it. You're a BLOWFISH! </p>
-                <p>My partner was about to get himself shot. I intervened. He was angry because those two dealers of yours had just murdered an eleven year-old boy. Then again, maybe he thought it was you who gave the order. </p>
+                <p class="bio-text">It's such a tiring job but someone has to do it.</p>
             </div>
             <div class="container">
                 <img class="bio-pic" id="right-pic" src="../img/clairepic.png">
 
-                <p>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I'm breaking now. We said we'd say it was the snow that killed the other two, but it wasn't. Nature is lethal but it doesn't hold a candle to man. </p>
+                <p>I'm an expert in ladders.</p>
 
-                <p>My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there. Then I'm gonna shoot that bitch in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey, look at me when I'm talking to you, motherfucker. You listen: we go in there, and that nigga Winston or anybody else is in there, you the first motherfucker to get shot. You understand? </p>
+                <p>Since the first time I saw a ladder, I knew.</p>
 
-                <p>Do you see any Teletubbies in here? Do you see a slender plastic tag clipped to my shirt with my name printed on it? Do you see a little Asian child with a blank expression on his face sitting outside on a mechanical helicopter that shakes when you put quarters in it? No? Well, that's what you see at a toy store. And you must think you're in a toy store, because you're here shopping for an infant named Jeb. </p>
+                <p>Also... I LOVE buckets. Mr. Bucket is my secret crush!</p>
+
+                <img class="bio-pic" id="right-pic" src="../img/mr-bucket.jpg">
 
                 <p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass. </p>
-
-                <p>We have so many things that we have to do better... and certainly ipsum is one of them. I will write some great placeholder text – and nobody writes better placeholder text than me, believe me – and I’ll write it very inexpensively. I will write some great, great text on your website’s Southern border, and I will make Google pay for that text. Mark my words. I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things.</p>
-                <p>My placeholder text, I think, is going to end up being very good with women. When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good words. You could see there was text coming out of her eyes, text coming out of her wherever. It’s about making placeholder text great again. That’s what people want, they want placeholder text to be great again.</p>
-                <p>The other thing with Lorem Ipsum is that you have to take out its family. I will write some great placeholder text – and nobody writes better placeholder text than me, believe me – and I’ll write it very inexpensively. I will write some great, great text on your website’s Southern border, and I will make Google pay for that text. Mark my words. Look at these words. Are they small words? And he referred to my words - if they're small, something else must be small. I guarantee you there's no problem, I guarantee. </p>
-                <p>My placeholder text, I think, is going to end up being very good with women. Lorem Ipsum is FAKE TEXT! Look at these words. Are they small words? And he referred to my words - if they're small, something else must be small. I guarantee you there's no problem, I guarantee.</p>
             </div>
             <div class="container">
                 <img class="bio-pic" src="../img/ronpic.png">
 
-                <p class="bio-text">I don't think we're alike at all, Mr. White. You are not a cautious man at all. Your partner was late and he was high. He's high often, isn't he? You have poor judgement. I can't work with someone with poor judgement. I've been told, it's excellent. That is not the only factor. I have to ask... why? Why him? How much product do you have on hand? I have your numbers. You can never trust a drug addict. </p>
+                <p class="bio-text">I'm an expert in love.</p>
 
-                <p class="bio-text">Right now, you deal almost exclusively in cocaine. But the coca plant can't grow in Mexico. You are no more than middlemen for the Colombians. They cut you a percentage for smuggling across the border. But the lion's share of the profit goes to them, despite your assumption of all the risk. And you keep all the profits. </p>
+                <p class="bio-text">I'm on my 6th marriage so clearly I'm an expert in love.</p>
 
-                <p class="bio-text">What kind of man talks to the DEA? No man. No man at all. A crippled little rata. What a reputation to leave behind. Is that how you want to be remembered? Last chance to look at me, Hector. Can I help you sir? What can I do for you? I'm... sorry, I'm not following. I... think that you're confusing me for someone else. Sir, if you have a complaint, I suggest you submit it through our email system. I will be happy to refer you to our website. </p>
-
-                <p>Something incredible is waiting to be known at the edge of forever hundreds of thousands ship of the imagination finite but unbounded birth. Citizens of distant epochs emerged into consciousness great turbulent clouds made in the interiors of collapsing stars muse about invent the universe. Paroxysm of global death vanquish the impossible vastness is bearable only through love permanence of the stars intelligent beings venture. The only home we've ever known the ash of stellar alchemy from which we spring a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing with pretty stories for which there's little good evidence?</p>
-                <p>Laws of physics the only home we've ever known a billion trillion courage of our questions citizens of distant epochs ship of the imagination. A still more glorious dawn awaits stirred by starlight a still more glorious dawn awaits a still more glorious dawn awaits not a sunrise but a galaxyrise network of wormholes. Star stuff harvesting star light descended from astronomers star stuff harvesting star light permanence of the stars at the edge of forever tendrils of gossamer clouds.</p>
-                <p>I think the only difference between me and the other placeholder text is that I’m more honest and my words are more beautiful. Lorem Ipsum is FAKE TEXT! You know, it really doesn’t matter what you write as long as you’ve got a young, and beautiful, piece of text.</p>
-                <p>I have been waiting. I've been waiting all day. Waiting for Gus to send one of his men to kill me. And it's you. Who do you know, who's okay with using children, Jesse? Who do you know... who's allowed children to be murdered... hmm? Gus! He has, he has been ten steps ahead of me at every turn. And now, the one thing that he needed to finally get rid of me is your consent and boy he's got that now. He's got it. And not only does he have that, but he manipulated you into pulling the trigger for him.</p>
-                <p>You're his cook now. You're the cook and you have proven that you can run a lab without me and now that cook has reason to kill me. Think about it! It's brilliant. So go ahead. If you think that I am capable of doing this, then go ahead. Put a bullet, in my head, and kill me right now. DO IT! Do it. Do it. Do it.</p>
+                <p class="bio-text">I want to help you achieve better success than Brangelina!</p>
             </div>
         </main>
         <div class="footer">
