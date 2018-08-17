6bc7ac183693ea8b0bb5150e16b1c7d6cefc2557
warrioryoko <miangpellegri@gmail.com>
2018-08-17 11:09:44 -0700

    Finally got the about us page looking decent, and responsive.

--- js/components/aboutus-app.js
+++ js/components/aboutus-app.js
@@ -7,34 +7,33 @@ let template = function() {
         <div class="header">
 
         </div>
-        <main>
+        <main class="aboutme-content">
             <div class="container">
-                <img class="bio-pic" src="img/laurenpic.png">
+                <div class="bio-pic-combo">
+                    <img class="bio-pic" src="img/laurenpic.png">
 
-                <p class="bio-text">I'm an expert in celebrities.</p>
+                    <p class="bio-text">I'm an expert in celebrities.</p>
 
-                <p class="bio-text">I spend all of my time reading Us Weekly and TMZ.</p>
-
-                <p class="bio-text">It's such a tiring job but someone has to do it.</p>
+                    <p class="bio-text">I spend all of my time reading Us Weekly and TMZ. It's such a tiring job but someone has to do it.</p>
+                </div>
             </div>
             <div class="container">
-                <img class="aboutus-right" src="img/clairepic.png">
+                <div class="bio-pic-combo">
+                    <img class="bio-pic" src="img/clairepic.png">
 
-                <p>I'm an expert in ladders.</p>
+                    <p class="bio-text">I'm an expert in ladders.</p>
 
-                <p>Since the first time I saw a ladder, I knew.</p>
-
-                <p>Also... I LOVE buckets. Mr. Bucket is my secret crush!</p>
-
+                    <p class="bio-text">Since the first time I saw a ladder, I knew this would be my destiny.</p>
+                </div>
             </div>
             <div class="container">
-                <img class="bio-pic" src="img/ronpic.png">
+                <div class="bio-pic-combo">
+                    <img class="bio-pic" src="img/ronpic.png">
 
-                <p class="bio-text">I'm an expert in love.</p>
+                    <p class="bio-text">I'm an expert in love.</p>
 
-                <p class="bio-text">I'm on my 6th marriage so clearly I'm an expert in love.</p>
-
-                <p class="bio-text">I want to help you achieve better success than Brangelina!</p>
+                    <p class="bio-text">I'm on my 6th marriage. I want to help you achieve the same success I have!</p>
+                </div>
             </div>
         </main>
         <div class="footer">
